9b07af52804c37883a93a5dfcb88905020121a6c
fikrymaulana25 <116711023+fikrymaulana25@users.noreply.github.com>
2022-10-26 14:32:44 +0700

    upload web
    upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const txtElement = ['Web Developer', 'Graphic Designer'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+    console.log(txtElement[0]);
+
+})();
